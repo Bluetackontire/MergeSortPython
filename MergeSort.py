0a0c91a42e568e142ccfba11f94cd1749dd88dd8
Bluetackontire <107205542+Bluetackontire@users.noreply.github.com>
2024-12-01 11:12:19 +0000

    completed merge sort program for python

--- MergeSort.py
+++ MergeSort.py
@@ -1,6 +1,6 @@
 def MergeSort(A , low, high):
     if low < high :
-        mid = (low + high) // 2
+        mid = low + (high - low) // 2 
         MergeSort(A, low, mid)
         MergeSort(A, mid + 1, high)
         Merge(A, low, mid, high)
@@ -9,19 +9,23 @@ def Merge(A , low, mid, high):
     #Calculate sizes of subarrays
     n1 = mid - low + 1
     n2 = high - mid
+    
+    Larray = [0] * n1
+    Rarray = [0] * n2
 
     #Create temporary arrays to hold data
-    for i in range(n1):
-        Larray[i] = A[low + i - 1]
-    for j in range (n2):
-        Rarray[j] = A[mid+j]
+    for i in range(0, n1):
+        Larray[i] = A[low + i]
+    for j in range (0, n2):
+        Rarray[j] = A[mid + j]
+        
 
     #Merge Two subarrays back into original array
-    i = 1
-    j = 1
-    k = low
+    i = 0
+    j = 0
+    k = 1
 
-    while (i <= n1) and (j <= n2):
+    while (i < n1) and (j < n2):
         if Larray[i] <= Rarray[j]:
             A[k] = Larray[i]
             i = i + 1 
@@ -31,18 +35,21 @@ def Merge(A , low, mid, high):
         k = k + 1
 
     #Copy remaining elements in Larray
-    while i <= n1:
+    while i < (n1):
         A[k] = Larray[i]
         i = i + 1
         k = k + 1
     
     #Copy elements of Rarray
-    while j <= n2:
+    while j < n2:
         A[k] = Rarray[j]
         j = j + 1
         k = k + 1
 
-S = Arr.length
-MergeSort(Arr, 1, S)
-for i in range(S):
-    print(Arr[i])
+Arr = [8, 13, 6, 20, 11, 2, 7, 16]
+Larray = []
+Rarray = []
+S = len(Arr)
+MergeSort(Arr, 0, S)
+for i in range (len(Arr)):
+    print(Arr[i], " ")
